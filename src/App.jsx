a426cc547e2a7d61180a238885a002de4ecfafe9
k1nnyyY <osman.haciev231@gmail.com>
2023-11-24 14:40:26 +0400

    com

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Auth from "./pages/Auth/Auth";
+import OneModal from "./pages/ResetPassword/OneModal/OneModal";
+import SecondModal from "./pages/ResetPassword/SecondModal/SecondModal";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <GoogleOAuthProvider clientId="562637798189-br1bjlrul0vo4qin18l2uaq7u2q5m759.apps.googleusercontent.com">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/reset-to-email" element={<OneModal />} />
+        <Route path="/success-to-email" element={<SecondModal />} />
+      </Routes>
+    </Router>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/ResetPassword/SecondModal/SecondModal.jsx
@@ -0,0 +1,35 @@
+import styles from "./SecondModal.module.css";
+import { React } from "react";
+import mail from "../../../assets/mail.svg";
+
+const SecondModal = () => {
+  return (
+    <div>
+      <div>
+        <div className={styles.login_wrapper}>
+          <div className={styles.content_wrapper}>
+            <img className={styles.mail} src={mail} />
+            <h2
+              style={{
+                fontWeight: "500",
+                textAlign: "center",
+                marginBottom: "-30px",
+              }}
+            >
+              Отправлено!
+            </h2>
+            <h6 className={styles.title}>
+              Мы отправили вам по электронной почте ссылку <br />
+              для сброса пароля.
+            </h6>
+            <p className={styles.no_title}>
+              <span className={styles.no_main}>Назад к логину</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondModal;
